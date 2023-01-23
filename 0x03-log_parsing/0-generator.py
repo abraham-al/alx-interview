21d428e87244c53a9a9a184b6112ca6856648697
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2023-01-23 13:42:32 +0300

    log

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-'''a scvript that reads jhagdh stdin line by line and computes metrics'''
-
 import random
 import sys
 from time import sleep
@@ -14,4 +12,3 @@ for i in range(10000):
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
-    sys.stdout.flush()
